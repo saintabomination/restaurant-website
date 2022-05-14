99d3020cf78bd4368d8709557908afcfe2c9c067
saintabomination <saint.abomination@protonmail.com>
2022-05-14 10:54:08 +0200

    Adding and setting up xstyled

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,22 @@ import App from './App';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import { ThemeProvider } from '@xstyled/styled-components';
+import mainTheme from './theme/mainTheme';
+import ResetStyles from './theme/resetStyles';
+import MainStyle from './theme/mainStyle';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <ThemeProvider theme={mainTheme}>
+        <ResetStyles />
+        <MainStyle />
+        <App />
+      </ThemeProvider>
     </Router>
   </React.StrictMode>
 );
